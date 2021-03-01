8cd6bc5752a1662f7dfb7725a7010e18455a18e2
Brahim Chahbouni <brahimcah@gmail.com>
2021-03-01 17:05:54 +0100

    Update Stock.java
    Actualitzo el nom de les

--- Stock.java
+++ Stock.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Stock {
     
     public static void main(String[] args) throws Exception {
+        //Noms dels Productes
         String[] NomProducte = new String[10];
         NomProducte[0] = "Chips Casolanes";
         NomProducte[1] = "Xocolata amb Ametlles";
@@ -10,8 +11,8 @@ public class Stock {
         NomProducte[3] = "Aigua 50ml";
         NomProducte[4] = "ColaCao ml";
         NomProducte[5] = "Galetes Veganes";
-        NomProducte[6] = "";
-        NomProducte[7] = "Chips Casolanes";
-        NomProducte[8] = "Chips Casolanes";
-        NomProducte[9] = "Chips Casolanes";
+        NomProducte[6] = "Sanwitch de ";
+        NomProducte[7] = "Filipinos";
+        NomProducte[8] = "Magdalena";
+        NomProducte[9] = "Palmera";
     }
