9214fd5662f2dfe5ed03ab8403497ec528d66581
Brahim Chahbouni <brahimcah@gmail.com>
2021-03-02 00:24:47 +0100

    Afegim els preus dels Productes

--- Stock.java
+++ Stock.java
@@ -11,8 +11,23 @@ public class Stock {
         NomProducte[3] = "Aigua 50ml";
         NomProducte[4] = "ColaCao ml";
         NomProducte[5] = "Galetes Veganes";
-        NomProducte[6] = "Sanwitch de ";
+        NomProducte[6] = "Sanwitch de Pavo";
         NomProducte[7] = "Filipinos";
         NomProducte[8] = "Magdalena";
         NomProducte[9] = "Palmera";
+    }
+
+    static void NomsProductes(double[] NomsProductes){
+        //Noms dels Productes
+        double  [] NomProducte = new double [10];
+        NomProducte[0] = 1.50;
+        NomProducte[1] = 1;
+        NomProducte[2] = 2;
+        NomProducte[3] = 0.75;
+        NomProducte[4] = 1.85;
+        NomProducte[5] = 1.55;
+        NomProducte[6] = 2.50;
+        NomProducte[7] = 1;
+        NomProducte[8] = 1.10;
+        NomProducte[9] = 1.25;
     }
