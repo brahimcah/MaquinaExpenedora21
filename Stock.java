fd7284ad7653ae54439297478fb1dc7d3fc90402
Brahim Chahbouni <brahimcah@gmail.com>
2021-03-01 17:00:30 +0100

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -1,1 +1,17 @@
+import java.util.*;
 
+public class Stock {
+    
+    public static void main(String[] args) throws Exception {
+        String[] NomProducte = new String[10];
+        NomProducte[0] = "Chips Casolanes";
+        NomProducte[1] = "Xocolata amb Ametlles";
+        NomProducte[2] = "Coca-Cola";
+        NomProducte[3] = "Aigua 50ml";
+        NomProducte[4] = "ColaCao ml";
+        NomProducte[5] = "Galetes Veganes";
+        NomProducte[6] = "";
+        NomProducte[7] = "Chips Casolanes";
+        NomProducte[8] = "Chips Casolanes";
+        NomProducte[9] = "Chips Casolanes";
+    }
