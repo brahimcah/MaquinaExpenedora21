e8d22644b92f58003907921a82b1b35570f983a1
alex <alexlopez@inspladelestany.cat>
2021-03-02 19:17:42 +0100

    fet fins el switch de mateniment

--- Manteniment.java
+++ Manteniment.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
+public class Manteniment{
+    public static void manteniment(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Opcio 1: Accedir a repostar l'estoc");
+        System.out.println("Opcio 2: Accedir al monader");
+        System.out.println("Opcio 10: Sortir de la aplicació");
+        System.out.println();
+        System.out.print("Seleccioni una opció de les tres opcions: ");
+        int opcio = 0;
 
+        do{
+            opcio = sc.nextInt();
+            switch(opcio){
+                case 1: repostarStock();
+                case 2: repostarMoneder();
+                case 10: System.exit(0);
+            }
+        }while(opcio!=1 && opcio!=2 && opcio!=10);
+
+
+    }
+    public static void repostarStock(){
+        System.out.println("REPSOTAR STOCK");
+    }
+    public static void repostarMoneder(){
+
+    }
+}
--- application.java
+++ application.java
@@ -8,23 +8,29 @@ public class application{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         impressio();
-        System.out.print("Sisplau introdueix el codi del producte: ");
+        boolean infinit = true;
         int codi = 0;
 
         do{
+            System.out.print("Sisplau introdueix el codi del producte: ");
             codi=sc.nextInt();
+
+            if(codi!=99 || (codi<0 && codi>9)){
+                System.out.println("*********************ERROR*********************");
+                System.out.println("El codi introduït és incorrecte.");
+                System.out.print("Sisplau introdueix el codi del producte: ");
+                codi=sc.nextInt();
+            }
 
             if(codi==99){ //manteniment
                 System.out.print("Introdueix la contrasenya: ");
                 int contrasenya = sc.nextInt();
                 if(contrasenya==9876){
-                    // Manteniment.manteniment();
+                    Manteniment.manteniment();
                 }else{
                     System.out.print("Contrasenya incorrecta.");
                     System.exit(0);
                 }
-                //manteniment.manteniment;
-                System.out.println("Manteniment");
             }else{
                 if(Stock.nomProducte(codi)){//Comprovació de Stock, en cas d'haver Stock es seguirà amb el programa
                     if(Diners.diners(codi)>=0){//Comprovació de diners a introduir, en cas d'haver canvi i una introducció d'aquests correcte es retorna el producte
@@ -40,7 +46,7 @@ public class application{
                     codi = 10;
                 }
             }
-        }while(codi!=99 || (codi<0 && codi>9));
+        }while(infinit);
     }
 }
 
