97182709ae9b5ebb108ff2050de1edf7124ff2ce
manelpagesins <manelpages@inspladelestany.cat>
2021-03-02 16:36:41 +0100

    Funcio de diners i afegit de els methods

--- Diners.java
+++ Diners.java
@@ -1,1 +1,55 @@
+import java.util.Scanner;
 
+public class Diners{
+
+
+    public static int diners(int codi){
+        
+        /**
+         * 
+         * @method Impressio de pantalla del preu del producte i solicita el preu que vol introduir el client
+         */
+
+        Scanner lector = new Scanner(System.in);
+
+
+        int diners = 0;
+
+        System.out.print("El preu del producte es" + stock.ComprovacioPreuProducte(int codi));
+
+
+        System.out.print("Quants diners vol gastar?");
+        diners = lector.nextInt();
+
+        return diners;
+    }
+
+    public static int canvi(int preu){
+
+
+
+        /**
+         * @method Comprovacio de si hem de tornar canvi al nostre client
+         * 
+         */
+
+        
+
+
+
+
+
+    }
+
+    public static int moneder(int preu){
+
+        /**
+         * @method 
+         * 
+         */
+
+
+
+    }
+
+}
