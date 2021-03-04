b7a2f48cc50e67f3f2824f47614ed7ad9578d801
manelpagesins <manelpages@inspladelestany.cat>
2021-03-04 17:23:43 +0100

    Modificacio de la funcio diners + comentaris dins del fitxer

--- Diners.java
+++ Diners.java
@@ -15,38 +15,69 @@ public class Diners{
 
         int diners = 0;
 
-        System.out.print("El preu del producte es" + stock.ComprovacioPreuProducte(int codi));
+        System.out.print("El preu del producte es" + Stock.ComprovacioPreuProducte(int codi)); //Impressio de el preu del producte seleccionat
 
+        System.out.print("Nomes acceptem les monedes següents"); //Imprimim les uniques monedes que acceptem per que es pugui seleccionar un dels nostres productes
+        System.out.println("Posició 0: Monedes de 2,00€");
+        System.out.println("Posició 1: Monedes de 1,00€");
+        System.out.println("Posició 2: Monedes de 0,50€");
+        System.out.println("Posició 3: Monedes de 0,20€");
+        System.out.println("Posició 4: Monedes de 0,10€");
+        System.out.println("Posició 5: Monedes de 0,05€");
 
-        System.out.print("Quants diners vol gastar?");
-        diners = lector.nextInt();
+        System.out.print("Introdueix el numero de monedes que vol gastar?");
+        diners = lector.nextInt(); //Preguntem al usuari per quantes monedes vol gastar
+
+        
+
 
         return diners;
     }
 
-    public static int canvi(int preu){
+    public static int canvi(int preu, int diners){
 
-
-        
         /**
          * @method Comprovacio de si hem de tornar canvi al nostre client
          * 
          */
 
 
+        int restant = 0; 
+                
+         restant = diners - Stock.ComprovacioPreuProducte(int codi); //  Assignem a la variable creada  la operacio per poder determinar si hem de tornar canvi o no
 
-
-
-
+        return restant;
 
     }
 
     public static int moneder(int preu){
 
+        Scanner lector = new Scanner(System.in);
+
+    int dinsMoneder = 0;  //Variable per poder detectar els dieners que hi han dins del moneder 
+    int introduirMonedes = 0;  // Variable per si es dones el cas de que ha de introduir diners per poder acabar de pagar el producte
+        
+    dinsMoneder = canvi(int preu, int diners);
+
+        if(dins < 0)
+        {
+            System.out.println("Falta pagar" + dinsMoneder(int preu, int diners));
+
+            System.out.println("Introdueix els diners que creguis convenients");
+                introduirMonedes = lector.nextInt(); //Solicitem al usuari quantes monedes vol introduir
+        }else
+        {
+
+        }
+
+
         /**
          * @method 
          * 
          */
+
+    
+
 
 
 
