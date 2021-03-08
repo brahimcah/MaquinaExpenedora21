e63cd4d84655f54e6fbb09d1c8aa928b68c0f8fd
manelpagesins <manelpages@inspladelestany.cat>
2021-03-08 15:45:07 +0100

    update de diners.java

--- Diners.java
+++ Diners.java
@@ -15,20 +15,75 @@ public class Diners{
 
         int diners = 0;
 
+        double EurosAmbCent = 0;
+
         System.out.print("El preu del producte es" + Stock.ComprovacioPreuProducte(int codi)); //Impressio de el preu del producte seleccionat
 
         System.out.print("Nomes acceptem les monedes següents"); //Imprimim les uniques monedes que acceptem per que es pugui seleccionar un dels nostres productes
-        System.out.println("Posició 0: Monedes de 2,00€");
-        System.out.println("Posició 1: Monedes de 1,00€");
-        System.out.println("Posició 2: Monedes de 0,50€");
-        System.out.println("Posició 3: Monedes de 0,20€");
-        System.out.println("Posició 4: Monedes de 0,10€");
-        System.out.println("Posició 5: Monedes de 0,05€");
+        System.out.println("Posició 0: Moneda de 2,00€");
+        System.out.println("Posició 1: Moneda de 1,00€");
+        System.out.println("Posició 2: Moneda de 0,50€");
+        System.out.println("Posició 3: Moneda de 0,20€");
+        System.out.println("Posició 4: Moneda de 0,10€");
+        System.out.println("Posició 5: Moneda de 0,05€");
 
         System.out.print("Introdueix el numero de monedes que vol gastar?");
-        diners = lector.nextInt(); //Preguntem al usuari per quantes monedes vol gastar
+        diners = lector.nextInt(); //Preguntem al usuari per quantes monedes vol gastar, aixi obtindrem el limit de monedes que introduïra el nostre usuari
 
-        
+        double[] IndexMonedes = new double[6]; //Assignem les dades del array manualment per despres identificar el tipus de moneda
+
+            IndexMonedes[0] = 2,00;
+            IndexMonedes[1] = 1,00;
+            IndexMonedes[2] = 0,50;
+            IndexMonedes[3] = 0,20;
+            IndexMonedes[4] = 0,10;
+            IndexMonedes[5] = 0,05;
+
+
+            int[] MonedesEntrades = new int[diners];
+
+        //Creem un for per poder recollir la informacio de les monedes que introdueix el nostre usuari
+        for(int i = 0; i < MonedesEntrades.length; i++){
+
+                System.out.println("Selecciona les monedes que desitjes entrar a la maquina(0-5): ");
+                 MonedesEntrades[i] = lector.nextDouble(); //
+
+                switch (MonedesEntrades) { //Creacio de un switch per poder diferenciar les diferentes opcions entrades del usuari amb el index de Monedes i despres assiganr 
+
+                    case 0:
+
+                        EurosAmbCent + IndexMonedes[0]; 
+
+                        break;
+
+                    case 1:
+                        EurosAmbCent + IndexMonedes[1];
+                        break;
+
+                    case 2:
+                        EurosAmbCent + IndexMonedes[2];
+                        break;
+
+                    case 3:
+                        EurosAmbCent + IndexMonedes[3];
+                        break;
+
+                    case 4:
+                        EurosAmbCent + IndexMonedes[4];
+                        break;
+
+                    case 5:
+                        EurosAmbCent + IndexMonedes[5];
+
+                    default:
+
+                        break;
+                }
+
+
+            }
+
+        }
 
 
         return diners;
